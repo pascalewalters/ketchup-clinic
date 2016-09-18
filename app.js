73f4461d2c7b2d02bbcecb269b46c129c7b30f72
seanlazaro <seanchristienlazaro@gmail.com>
2016-09-18 10:13:20 -0400

    Moved main page index.html to root

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ $(document).ready(function() {
 
 		var username = $("[aria-describedby='username-addon']").val();
 		if(username == 'dr.mario'){
-			window.location.href = '../hospital-page/index.html';
+			window.location.href = 'hospital-page/index.html';
 		}
 		else if(username == 'alisha.castillo'){
-			window.location.href = '../patient-profile/index.html';
+			window.location.href = 'patient-profile/index.html';
 		}
 		else{
-			window.location.href = '../Village People/index.html';
+			window.location.href = 'Village People/index.html';
 		}
 		
 	});
